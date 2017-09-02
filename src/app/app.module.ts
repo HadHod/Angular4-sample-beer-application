4bc939d1906f4c49fc983732df9d703cbbc4496f
Maciej Młynarski <maciej.mlynarski@gmail.com>
2017-09-02 13:41:59 +0200

    v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MdToolbarModule } from '@angular/material';
+
+
+const MD_COMPONENTS = [MdToolbarModule];
+const DIRECTIVES = [];
+const PIPES = [];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        MD_COMPONENTS, DIRECTIVES, PIPES
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
